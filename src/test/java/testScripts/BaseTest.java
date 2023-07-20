90187bf2da7971e3054c07e8273d67bf75710d67
AKNJr <aknjr4431@gmail.com>
2023-07-20 23:48:56 +0530

    change

--- src/test/java/testScripts/BaseTest.java
+++ src/test/java/testScripts/BaseTest.java
@@ -3,6 +3,7 @@ package testScripts;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -15,7 +16,7 @@ public class BaseTest {
     protected WebDriver driver;
     @BeforeMethod
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup(); 
         driver = new ChromeDriver();
         driver.navigate().to("https://www.hashtag-ca.com/careers/apply?jobCode=QAE001");
     }
